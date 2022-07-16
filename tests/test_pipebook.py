fdc08238ace931c33b68ca8370c5615907798c11
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-15 17:55:18 -0700

    test_registry

--- tests/test_pipebook.py
+++ tests/test_pipebook.py
@@ -1,8 +1,15 @@
 from pipebook import __version__
 from .conftest import *
+from fridaay import PKG_ID
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_app():
     app = PipeBookApp(APP_NAME, APP_ID)
+    assert app
+
+def test_registry():
+    app = PipeBookApp(APP_NAME, APP_ID)
+    app.startup()
+    assert app.registry
