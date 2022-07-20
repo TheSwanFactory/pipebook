9013538368873e06cedd629cb18e159e80ab1324
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-20 15:54:21 -0700

    from fridaay import PKG_ID

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
 )))
 
 from pipebook import *
+from fridaay import *
--- tests/test_pipebook.py
+++ tests/test_pipebook.py
@@ -1,6 +1,5 @@
 from pipebook import __version__
 from .conftest import *
-from fridaay import PKG_ID
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -13,3 +12,10 @@ def test_registry():
     app = PipeBookApp(APP_NAME, APP_ID)
     app.startup()
     assert app.registry
+
+def test_yaml():
+    assert PKG_ID
+    path = path_package(PKG_ID)
+    assert PKG_ID in str(path)
+    pipe_path = path_resource(PKG_ID, PIPE_FOLDER)
+    assert pipe_path
