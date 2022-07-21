d96db72e77572041e1b1a92f605156a0ef2f1d03
Ernest Prabhakar <ernest.prabhakar@gmail.com>
2022-07-21 16:55:41 -0700

    INDEX_KEY

--- pipebook/data.py
+++ pipebook/data.py
@@ -3,7 +3,7 @@ import toga
 from toga.constants import COLUMN, ROW
 from toga.sources import Source
 import numpy as np
-INDEX_KEY="#"
+INDEX_KEY="Index"
 
 class FrameData(Source):
 
@@ -11,16 +11,16 @@ class FrameData(Source):
         super().__init__()
         self.raw = raw
         self.columns = raw.columns.to_list()
-        self.struct = namedtuple("row", ['index']+self.columns)
+        self.struct = namedtuple("row", [INDEX_KEY]+self.columns)
         self.data = [self.struct(*r) for r in self.raw.itertuples()]
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        print('__getitem__',index)
+        print('__getitem__', index)
         return self.data[index]
 
     def index(self, entry):
-        print('index',entry)
+        print(INDEX_KEY, entry)
         return self.data.index(entry)
